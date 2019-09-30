cead520ac932aad4f38de0d605b0597306951883
František Milt <frantisek.milt@gmail.com>
2019-09-30 17:35:48 +0200

    Reworked copyright notice, added library-specific exception for better exception handling

--- StaticMemoryStream.pas
+++ StaticMemoryStream.pas
@@ -9,17 +9,34 @@
 
   StaticMemoryStream
 
-  ©František Milt 2017-08-01
+    Very simple class intended to provide a way of accessing general memory
+    location using usual streams.
+    Unlike TMemoryStream, this implementation will not realocate the memory
+    (an exception is raised when attempted), so it is safe to use it on
+    pointers from external sources (libraries, OS, ...).
 
-  Version 1.0
+  Version 1.0 (2017-08-01)
 
-  Very simple class intended to provide a way of accessing general memory
-  location using usual streams.
-  Unlike TMemoryStream, this implementation cannot realocate the memory, so it
-  is safe to use it on pointers from external sources (libraries, OS, ...).
+  Last change 2019-09-30
+
+  ©2017-2019 František Milt
+
+  Contacts:
+    František Milt: frantisek.milt@gmail.com
+
+  Support:
+    If you find this code useful, please consider supporting its author(s) by
+    making a small donation using the following link(s):
+
+      https://www.paypal.me/FMilt
+
+  Changelog:
+    For detailed changelog and history please refer to this git repository:
+
+      github.com/TheLazyTomcat/Lib.StaticMemoryStream
 
   Dependencies:
-    AuxTypes  - github.com/ncs-sniper/Lib.AuxTypes
+    AuxTypes - github.com/TheLazyTomcat/Lib.AuxTypes
 
 ===============================================================================}
 unit StaticMemoryStream;
@@ -33,17 +50,25 @@ unit StaticMemoryStream;
 interface
 
 uses
-  Classes, AuxTypes;
+  SysUtils, Classes,
+  AuxTypes;
 
-{==============================================================================}
-{------------------------------------------------------------------------------}
-{                             TStaticMemoryStream                              }
-{------------------------------------------------------------------------------}
-{==============================================================================}
+type
+  ESMSException = class(Exception);
 
-{==============================================================================}
-{   TStaticMemoryStream - declaration                                          }
-{==============================================================================}
+  ESMSCannotWrite  = class(ESMSException);
+
+  ESMSCannotChangeMemory = class(ESMSException);
+
+{===============================================================================
+--------------------------------------------------------------------------------
+                              TStaticMemoryStream
+--------------------------------------------------------------------------------
+===============================================================================}
+
+{===============================================================================
+    TStaticMemoryStream - declaration
+===============================================================================}
 
 type
   TStaticMemoryStream = class(TStream)
@@ -63,15 +88,15 @@ type
     property Size: TMemSize read fSize;
   end;
 
-{==============================================================================}
-{------------------------------------------------------------------------------}
-{                         TWritableStaticMemoryStream                          }
-{------------------------------------------------------------------------------}
-{==============================================================================}
+{===============================================================================
+--------------------------------------------------------------------------------
+                          TWritableStaticMemoryStream
+--------------------------------------------------------------------------------
+===============================================================================}
 
-{==============================================================================}
-{   TWritableStaticMemoryStream - declaration                                  }
-{==============================================================================}
+{===============================================================================
+    TWritableStaticMemoryStream - declaration
+===============================================================================}
 
   TWritableStaticMemoryStream = class(TStaticMemoryStream)
   public
@@ -80,28 +105,25 @@ type
 
 implementation
 
-uses
-  SysUtils;
-
 {$IFDEF FPC_DisableWarns}
   {$DEFINE FPCDWM}
   {$DEFINE W4055:={$WARN 4055 OFF}} // Conversion between ordinals and pointers is not portable
   {$DEFINE W5024:={$WARN 5024 OFF}} // Parameter "$1" not used
 {$ENDIF}
 
-{==============================================================================}
-{------------------------------------------------------------------------------}
-{                             TStaticMemoryStream                              }
-{------------------------------------------------------------------------------}
-{==============================================================================}
+{===============================================================================
+--------------------------------------------------------------------------------
+                              TStaticMemoryStream
+--------------------------------------------------------------------------------
+===============================================================================}
 
-{==============================================================================}
-{   TStaticMemoryStream - implementation                                       }
-{==============================================================================}
+{===============================================================================
+    TStaticMemoryStream - implementation
+===============================================================================}
 
-{------------------------------------------------------------------------------}
-{   TStaticMemoryStream - protected methods                                    }
-{------------------------------------------------------------------------------}
+{-------------------------------------------------------------------------------
+    TStaticMemoryStream - protected methods
+-------------------------------------------------------------------------------}
 
 Function TStaticMemoryStream.GetSize: Int64;
 begin
@@ -113,13 +135,13 @@ end;
 {$IFDEF FPCDWM}{$PUSH}W5024{$ENDIF}
 procedure TStaticMemoryStream.SetSize(const NewValue: Int64);
 begin
-raise Exception.Create('TStaticMemoryStream.SetSize: Cannot change size of static memory.');
+raise ESMSCannotChangeMemory.Create('TStaticMemoryStream.SetSize: Cannot change size of static memory.');
 end;
 {$IFDEF FPCDWM}{$POP}{$ENDIF}
 
-{------------------------------------------------------------------------------}
-{   TStaticMemoryStream - public methods                                       }
-{------------------------------------------------------------------------------}
+{-------------------------------------------------------------------------------
+    TStaticMemoryStream - public methods
+-------------------------------------------------------------------------------}
 
 constructor TStaticMemoryStream.Create(Memory: Pointer; Size: TMemSize);
 begin
@@ -158,7 +180,7 @@ begin
 {$IFDEF FPC}
 Result := 0;
 {$ENDIF}
-raise EWriteError.Create('TStaticMemoryStream.Write: Write operation not allowed on static memory.');
+raise ESMSCannotWrite.Create('TStaticMemoryStream.Write: Write operation not allowed on static memory.');
 end;
 {$IFDEF FPCDWM}{$POP}{$ENDIF}
 
@@ -175,19 +197,19 @@ Result := fPosition;
 end;
 
 
-{==============================================================================}
-{------------------------------------------------------------------------------}
-{                         TWritableStaticMemoryStream                          }
-{------------------------------------------------------------------------------}
-{==============================================================================}
+{===============================================================================
+--------------------------------------------------------------------------------
+                          TWritableStaticMemoryStream
+--------------------------------------------------------------------------------
+===============================================================================}
 
-{==============================================================================}
-{   TWritableStaticMemoryStream - implementation                               }
-{==============================================================================}
+{===============================================================================
+    TWritableStaticMemoryStream - implementation
+===============================================================================}
 
-{------------------------------------------------------------------------------}
-{   TWritableStaticMemoryStream - public methods                               }
-{------------------------------------------------------------------------------}
+{-------------------------------------------------------------------------------
+    TWritableStaticMemoryStream - public methods
+-------------------------------------------------------------------------------}
 
 Function TWritableStaticMemoryStream.Write(const Buffer; Count: LongInt): LongInt;
 begin

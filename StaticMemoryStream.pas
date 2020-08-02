5802dd5965c5f128e398678cc0b28d10c376c9a0
František Milt <frantisek.milt@gmail.com>
2020-08-02 22:13:33 +0200

    Explicitly activated long strings in all compilers and modes

--- StaticMemoryStream.pas
+++ StaticMemoryStream.pas
@@ -17,9 +17,9 @@
 
   Version 1.0 (2017-08-01)
 
-  Last change 2019-09-30
+  Last change 2020-08-02
 
-  ©2017-2019 František Milt
+  ©2017-2020 František Milt
 
   Contacts:
     František Milt: frantisek.milt@gmail.com
@@ -46,6 +46,7 @@ unit StaticMemoryStream;
   {$DEFINE FPC_DisableWarns}
   {$MACRO ON}
 {$ENDIF}
+{$H+}
 
 interface
 
